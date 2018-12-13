bf4b625b0ef78c8a08277e9f5614e83befa8fb9f
fronczek7f <fronczek7f@gmail.com>
2018-12-13 23:04:33 +0100

    Dodano klase OneStringItemActivity z prosta lista

--- app/src/main/java/com/android/fronc/project_06/OneStringItemActivity.java
+++ app/src/main/java/com/android/fronc/project_06/OneStringItemActivity.java
@@ -1,13 +1,34 @@
 package com.android.fronc.project_06;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class OneStringItemActivity extends AppCompatActivity {
+
+    private ListView lvListItems;
+    private String[] listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_string_item);
+
+        lvListItems = (ListView) findViewById(R.id.lvOneStringItem);
+        initResource();
+        initListView();
+    }
+
+    private void initResource() {
+        Resources resources = getResources();
+        listItems = resources.getStringArray(R.array.listItems);
+    }
+
+    private void initListView() {
+        lvListItems.setAdapter(new ArrayAdapter<String>(
+                getApplicationContext(), android.R.layout.simple_expandable_list_item_1, listItems)
+        );
     }
 }
