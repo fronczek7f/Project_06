697c83ef1f169d4821a3e3e150a7a05930b64c66
fronczek7 <fronczek7f@gmail.com>
2018-12-14 15:26:23 +0100

    Dodano wyswietlanie powiadomienia po nacisnieciu na element z listy

--- app/src/main/java/com/android/fronc/project_06/OneStringItemActivity.java
+++ app/src/main/java/com/android/fronc/project_06/OneStringItemActivity.java
@@ -3,8 +3,11 @@ package com.android.fronc.project_06;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class OneStringItemActivity extends AppCompatActivity {
 
@@ -30,5 +33,12 @@ public class OneStringItemActivity extends AppCompatActivity {
         lvListItems.setAdapter(new ArrayAdapter<String>(
                 getApplicationContext(), android.R.layout.simple_expandable_list_item_1, listItems)
         );
+
+        lvListItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), listItems[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
